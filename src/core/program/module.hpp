601e8a644b5a0be71150b7f6376ae3894d0e44c1
Glyn Leine <g.m.leine@gmail.com>
2023-04-14 22:23:24 +0200

    change some naming convention, remove broken template dependencies to program type for modules

--- src/core/program/module.hpp
+++ src/core/program/module.hpp
@@ -2,15 +2,14 @@
 #include "feature.hpp"
 
 namespace rythe::core {
-    template<typename ProgramT, feature_set_c FeatureSetT>
-    class Module {
+    template<feature_set_c FeatureSetT>
+    class module {
     public:
         using features = FeatureSetT;
     };
 
-    template<template<typename> typename... ModulesT>
-    struct Modules {
-        template<typename T>
-        using type_sequence = rsl::type_sequence<ModulesT<T>...>;
+    template<typename... ModulesT>
+    struct modules {
+        using type_sequence = rsl::type_sequence<ModulesT...>;
     };
 }
--- src/core/program/program.hpp
+++ src/core/program/program.hpp
@@ -2,9 +2,14 @@
 #include "module.hpp"
 
 namespace rythe::core {
-    template<typename This, template<typename> typename... ModulesT>
-    class Program {
+    template<typename... ModulesT>
+    class program {
     public:
-        using features = to_feature_set_t<rsl::concat_sequence_t<typename ModulesT<This>::features::type_sequence...>>;
+        using features = to_feature_set_t<rsl::concat_sequence_t<typename ModulesT::features::type_sequence...>>;
+
+        template<feature_c FeatureT>
+        static constexpr bool has_feature() {
+            return rsl::type_sequence_contains_v<typename features::type_sequence, FeatureT>;
+        }
     };
 }
